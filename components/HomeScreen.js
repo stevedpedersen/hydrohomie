039c2c63ebde4a9916fba8e999c56de3c0f9ccf9
Andrew Lesondak <andrewlesondak@Drewdaks-Mac.attlocal.net>
2020-06-13 12:34:10 -0700

    created new screens

---
+++ components/HomeScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, ImageBackground, Dimensions } from 'react-native';
+import { Text, Title, Paragraph, Button } from 'react-native-paper';
+
+class HomeScreen extends Component {
+    
+    render () {
+        return (
+            <View>
+                <Text>Home Screen</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
---
+++ components/Settings.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, ImageBackground, Dimensions } from 'react-native';
+import { Text, Title, Paragraph, Button } from 'react-native-paper';
+
+class Settings extends Component {
+    
+    render () {
+        return (
+            <View>
+                <Text>Settings Screen</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({});
+
+export default Settings;
