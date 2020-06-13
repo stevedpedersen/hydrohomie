9b303ac8d70638ed7f17a34cd35db38fb91c8b97
Andrew Lesondak <andrewlesondak@Drewdaks-Mac.attlocal.net>
2020-06-13 13:24:29 -0700

    added navigation and home page buttons

--- App.js
+++ App.js
@@ -1,80 +1,19 @@
-import React from 'react';
-import { StyleSheet, View, ImageBackground, Dimensions } from 'react-native';
-import { Text, Title, Paragraph, Button } from 'react-native-paper';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './components/HomeScreen';
+import Settings from './components/Settings';
 
-const deviceHeight = Dimensions.get('window').height;
-const deviceWidth = Dimensions.get('window').width;
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <ImageBackground source={require('./assets/images/icewater.jpg')} style={styles.image}>
-        <View style={styles.innerContainer}>
-          <View style={styles.card}>
-            <View style={styles.textContainer}>
-              <Text style={styles.text}>Hydro Homie</Text>
-            </View>
-            <View style={styles.buttonContainer}>
-              <Button
-                onPress={() => console.log('get hydrated')}
-                style={styles.button}
-                icon="cup-water"
-                mode="contained">
-              <Text style={{color: '#fff', fontWeight: 'bold'}}>Hydrate Me</Text>
-            </Button>
-            </View>
-          </View>
-        </View>
-      </ImageBackground>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column"
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Settings: Settings,
   },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-  },
-  innerContainer: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: 'center',
-  },
-  card: {
-    alignSelf: 'center',
-    paddingHorizontal: 20,
-    paddingVertical: 50,
-    marginVertical: deviceHeight / 3,
-    // marginHorizontal: 20,
-    // width: deviceWidth * 0.8,
-    width: '90%',
-    backgroundColor: 'rgba(255, 255, 255, .75)',
-    // opacity: .75,
-    borderRadius: 5,
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: 'space-between'
-  },
-  textContainer: {
-
-  },
-  text: {
-    textAlign: 'center',
-    color: "#000",
-    fontSize: 50,
-    fontWeight: "bold"
-  },
-  buttonContainer: {
-    
-  },
-  button: {
-    backgroundColor: '#61dafb',
-    paddingVertical: 6,
-    width: '90%',
-    alignSelf: 'center',
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: "Hydro Homie"
+    }
   }
-});
+);
+
+export default createAppContainer(navigator);
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -2,17 +2,88 @@ import React, { Component } from 'react';
 import { StyleSheet, View, ImageBackground, Dimensions } from 'react-native';
 import { Text, Title, Paragraph, Button } from 'react-native-paper';
 
+const deviceHeight = Dimensions.get('window').height;
+const deviceWidth = Dimensions.get('window').width;
+
 class HomeScreen extends Component {
     
-    render () {
-        return (
-            <View>
-                <Text>Home Screen</Text>
+  render () {
+      return (
+          <View style={styles.container}>
+          <ImageBackground source={require('../assets/images/icewater.jpg')} style={styles.image}>
+            <View style={styles.innerContainer}>
+              <View style={styles.card}>
+                <View style={styles.buttonContainer}>
+                  <Button
+                    onPress={() => console.log('get hydrated')}
+                    style={styles.button}
+                    icon="cup-water"
+                    mode="contained">
+                    <Text style={{color: '#fff', fontWeight: 'bold'}}>Hydrate Me</Text>
+                  </Button>
+                </View>
+                <View style={styles.buttonContainer}>
+                <Button
+                  onPress={() => this.props.navigation.navigate('Settings')}
+                  style={styles.button}>
+                    <Text style={{color: '#fff', fontWeight: 'bold'}}>Settings</Text>
+                  </Button>
+                </View>
+              </View>
             </View>
-        );
-    }
+          </ImageBackground>
+        </View>
+      );
+  }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "column"
+    },
+    image: {
+      flex: 1,
+      resizeMode: "cover",
+    },
+    innerContainer: {
+      flex: 1,
+      flexDirection: "column",
+      justifyContent: 'center',
+    },
+    card: {
+      alignSelf: 'center',
+      paddingHorizontal: 20,
+      paddingVertical: 20,
+      marginVertical: deviceHeight / 3,
+      // marginHorizontal: 20,
+      // width: deviceWidth * 0.8,
+      width: '90%',
+      backgroundColor: 'rgba(255, 255, 255, .75)',
+      // opacity: .75,
+      borderRadius: 5,
+      flex: 1,
+      flexDirection: "column",
+      justifyContent: 'space-between'
+    },
+    textContainer: {
+  
+    },
+    text: {
+      textAlign: 'center',
+      color: "#000",
+      fontSize: 50,
+      fontWeight: "bold"
+    },
+    buttonContainer: {
+      paddingVertical: 10
+    },
+    button: {
+      backgroundColor: '#61dafb',
+      paddingVertical: 6,
+      width: '90%',
+      alignSelf: 'center'
+    }
+  });
 
 export default HomeScreen;
